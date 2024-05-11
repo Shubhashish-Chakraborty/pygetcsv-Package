216107539fa6ef3e0bb425319240786d51c57f7c
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-05-11 15:29:15 +0530

    Functions Added - With issue - Will solve

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import os
 
 
-def convertToCSV(path , CSVname = "CSVFile"):
+def convertToCSV(path , CSVFileName = "CSVFile"):
 
     with open(path + ".dat", "rb") as f:
 
@@ -16,3 +16,63 @@ def convertToCSV(path , CSVname = "CSVFile"):
                 File_Data.append(pickle.load(f))
         except EOFError:
             pass
+
+
+    
+    #WRITING TITLE FOR THE CSV FILE
+
+    with open(CSVFileName + ".csv", "w", newline="") as f:
+
+
+        HeadingList = []
+        for d in range(len(File_Data)):
+
+
+            for k in range(len(File_Data[d])):
+                
+                
+                for h in File_Data[d]:
+
+                    HeadingList.append(h)
+                    
+        
+        CSVheading = HeadingList[0 : len(File_Data[d]) : 1]
+
+        wobjH = csv.writer(f)
+
+        wobjH.writerow(CSVheading)
+
+
+        with open(CSVFileName + ".csv", "a", newline="") as f:
+
+            wobjD = csv.writer(f)
+
+
+            for data in range(len(File_Data)):
+
+                AddDataList = []
+                for key in File_Data[data]:
+
+                    AddDataList.append(File_Data[data][key])
+
+                wobjD.writerow(AddDataList)
+
+
+def ReadFile(path):
+
+    with open(path + ".dat", "rb") as f:
+
+        try:
+            while True:
+                print(pickle.load(f))
+        except EOFError:
+            pass
+
+
+
+
+
+
+# convertToCSV("C:/codepurposes/ImportantFiles/SampleBin")
+
+# ReadFile("C:/codepurposes/ImportantFiles/SampleBin")
