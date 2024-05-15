ee32229db8e8a2e16a05ce814af46baf1a553125
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2024-05-16 00:40:48 +0530

    Main Function Added to Convert - Stable

--- main.py
+++ main.py
@@ -15,7 +15,67 @@ def ReadFile(path):
 
 
 
+def convertToCSV(path , CSVFileName = "CSVFile"):
 
-# convertToCSV("C:/codepurposes/ImportantFiles/SampleBin")
+    with open(path + ".dat", "rb") as f:
 
-# ReadFile("C:/codepurposes/ImportantFiles/SampleBin")
+        global File_Data
+        File_Data = []
+
+        try:
+            while True:
+                File_Data.append(pickle.load(f))
+        except EOFError:
+            pass
+
+
+    
+    #WRITING TITLE FOR THE CSV FILE
+
+    with open(CSVFileName + ".csv", "w", newline="") as f:
+
+
+        HeadingList = []
+        for d in range(len(File_Data)):
+
+
+            for k in range(len(File_Data[d])):
+                
+                
+                for h in File_Data[d]:
+
+                    HeadingList.append(h)
+                    
+        
+        CSVheading = HeadingList[0 : len(File_Data[d]) : 1]
+
+        wobjH = csv.writer(f)
+
+        wobjH.writerow(CSVheading)
+
+
+
+    #WRITING DATA FOR THE CSV FILE
+
+    with open(CSVFileName + ".csv", "a", newline="") as f:
+
+        wobjD = csv.writer(f)
+
+
+        for data in range(len(File_Data)):
+
+            AddDataList = []
+            for key in File_Data[data]:
+
+                AddDataList.append(File_Data[data][key])
+
+            wobjD.writerow(AddDataList)
+
+
+
+
+# convertToCSV("C:/codepurposes/ImportantFiles/SampleBin1")
+# convertToCSV("C:/codepurposes/ImportantFiles/SampleBin2")
+
+# ReadFile("C:/codepurposes/ImportantFiles/SampleBin1")
+# ReadFile("C:/codepurposes/ImportantFiles/SampleBin2")
